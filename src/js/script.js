a8b4ec6e085f08a722118bb4969ab49fe9b1ec05
ginger <kira.abramovich@gmail.com>
2020-03-17 13:15:24 +0300

    fix svg files

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1200,
 		adaptiveHeight: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_1.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/right_1.svg"></button>',
 		responsive: [
 			{
 				breakpoint: 992,
